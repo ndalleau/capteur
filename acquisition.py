2c6a80b4499c88eb9b2e9ab361ebba30da4d4d2c
ndalleau <46022524+ndalleau@users.noreply.github.com>
2019-09-13 22:23:38 +0200

    Update acquisition.py

--- acquisition.py
+++ acquisition.py
@@ -1,11 +1,26 @@
-import SDS011
+# Parseur ############################################################################
+#Le parseur definit les paramètres à compléter lors du lancement du script
+#Dans le cadre de ce script il s'agit uniquement du fichier de configuration fichier.ini
+description="""fichier de configuration, par defaut config.ini"""
+parseur=argparse.ArgumentParser(description=description)
+parseur.add_argument('-c','--config',dest='config',default='config.ini',help='fichier de configuration',type=str)
+
+args = parseur.parse_args()
+######################################################################################
+
+# Lecture du fichier de configuration.ini ############################################
+parser = configparser.ConfigParser()
+parser.read(args.config)
+
+######################################################################################
+
 
 #Variables #########################
 DEFAULT_SERIAL_PORT = "/dev/ttyUSB0"
 moyenne = 10
 
 
-
+import SDS011
 capteur = SDS011.SDS011(port = DEFAULT_SERIAL_PORT)
 
 while True:
